4e035772fc8dc20ae5bc6f95b24d1e34832914b8
kouamdo <kouamdoux@gmail.com>
2021-04-05 21:26:08 +0200

    remove useless code.Use it better after

--- src/kernel/process/task.c
+++ src/kernel/process/task.c
@@ -15,29 +15,7 @@ sheduler_t sheduler;
 
 static task_control_block_t main_task, task1, task2 , task3;
 
-void task1_()
-{
-    kprintf(2, 14, "task 1\n");
 
-    while (1)
-        ;
-}
-
-void task3_()
-{
-	kprintf(2, 14, "task 3\n");
-
-    while (1)
-        ;
-}
-
-void task2_()
-{
-    kprintf(2, 14, "task 2\n");
-
-    while (1)
-        ;
-}
 void __switch()
 {
     task_control_block_t* prev_task;
@@ -51,31 +29,6 @@ void __switch()
 
 void init_multitasking()
 {
-    // Get EFLAGS and cr3
-
-    asm volatile(
-        "movl %%cr3, %%eax;"
-        "movl %%eax, %0;"
-        : "=m"(main_task.regs.cr3)::"%eax");
-
-    asm volatile(
-        "pushfl;"
-        "pop %%eax;"
-        "movl %%eax , %0;"
-        : "=m"(main_task.regs.eflags)::"%eax");
-
-    create_task(&task1, task1_, main_task.regs.eflags, main_task.regs.cr3);
-    create_task(&task2, task2_, main_task.regs.eflags, main_task.regs.cr3);
-    create_task(&task3, task3_, main_task.regs.eflags, main_task.regs.cr3);
-
-    kprintf(2, 15, "[K:CPU]\tInit sheduling\n");
-
-    main_task.new_tasks = &task1;
-    task1.new_tasks = &task2;
-    task2.new_tasks = &task3;
-    task3.new_tasks = &main_task;
-
-    sheduler.running_task = &main_task;
     sheduler.init_timer = 0;
     sheduler.task_timer = DELAY_PER_TASK;
 }
