1522ad810a40f37efd35ca2f4ddf9208216bcb1f
kouamdo <kouamdoux@gmail.com>
2021-04-12 11:12:42 +0200

    Update console functionnalities

--- k_main.c
+++ k_main.c
@@ -1,16 +1,12 @@
 #define KERNEL__PAGE_MM
 #define KERNEL__Vir_MM
 
-#include <init/apic/apic.h>
-#include <init/apic/ioapic.h>
 #include <init/console.h>
 #include <init/gdt.h>
 #include <init/idt.h>
-#include <init/io.h>
-#include <init/paging.h>
-#include <lib.h>
-#include <mm.h>
-#include <task.h>
+#include <kernel/printf.h>
+
+extern void printf(const char* fmt, ...);
 
 void main()
 {
@@ -18,11 +14,11 @@ void main()
 
     cli;
 
-    cclean();
-
     init_gdt();
 
     init_idt();
+
+    init_console();
 
     sti;
 
--- src/init/console.c
+++ src/init/console.c
@@ -1,29 +1,38 @@
-#include "../../include/init/console.h"
-#include <stdarg.h>
+#include <init/console.h>
+#include <init/keyboard.h>
 #include <stdint.h>
-#include <init/io.h>
+
+#define MAX_ROWS 25
+#define MAX_COLS 80
 
 static int CURSOR_Y = 0;
 static int CURSOR_X = 0;
 
+void move_cursor(uint8_t x, uint8_t y);
+void show_cursor(void);
+
 void volatile cclean()
 {
     unsigned char* screen = (unsigned char*)(VIDEO_MEM);
-    int i = 0;
-    while (i != 160 * 24) {
-        screen[i] = ' ';
+    int            i      = 0;
+    while (i <= 160 * 25) {
+        screen[i]     = ' ';
         screen[i + 1] = 0x0;
         i += 2;
     }
-    cputchar(READY_COLOR , 'K');
-    cputchar(READY_COLOR , '>');
+    cputchar(READY_COLOR, 'K');
+    cputchar(READY_COLOR, '>');
+    cputchar(READY_COLOR, ' ');
+
+    CURSOR_X = 3;
+    CURSOR_Y = 0;
 }
 
 void volatile cscrollup()
 {
     unsigned char* v = (unsigned char*)(VIDEO_MEM + 3840);
-    unsigned char b[160];
-    int i;
+    unsigned char  b[160];
+    int            i;
     for (i = 0; i < 160; i++)
         b[i] = v[i];
 
@@ -39,53 +48,70 @@ void volatile cscrollup()
 
 void volatile cputchar(unsigned char color, unsigned const char c)
 {
-    unsigned char* v  ;
-    
-    if (c == '\n') {
-        CURSOR_X = 0;
-        CURSOR_Y++;
-    }
 
-    else if(c == 0x0D) {
-        CURSOR_X--;
-        v = (unsigned char*)(VIDEO_MEM + CURSOR_X * 2 + 160 * CURSOR_Y);
-        *v = ' ';
-    }
-
-    else if (c == '\t')
-        CURSOR_X += 5;
-
-    else {
-       v = (unsigned char*)(VIDEO_MEM + CURSOR_X * 2 + 160 * CURSOR_Y);
-
-        if (c == '\n' || CURSOR_X == 80) {
+    if ((CURSOR_Y) <= (25)) {
+        if (c == '\n') {
             CURSOR_X = 0;
             CURSOR_Y++;
-            *(v) = c;
-            *(v + 1) = color;
-            CURSOR_X++;
         }
+
+        else if (c == '\t')
+            CURSOR_X += 5;
+
+        else if (c == 0x08)
+
+        {
+            CURSOR_X--;
+            uint8_t* v = (uint8_t*)VIDEO_MEM;
+
+            v += 2 * CURSOR_X + 160 * CURSOR_Y;
+
+            *v = ' ';
+        }
+
         else {
-            *(v) = c;
-            *(v + 1) = color;
+            uint8_t* v = (uint8_t*)VIDEO_MEM;
+
+            v += 2 * CURSOR_X + 160 * CURSOR_Y;
+
+            *v = c;
+
+            *(v + 1) = READY_COLOR;
+
             CURSOR_X++;
         }
     }
+
+    else
+        cclean();
 }
 
-void move_cursor(uint8_t x , uint8_t y)
+void move_cursor(uint8_t x, uint8_t y)
 {
     uint16_t c_pos;
 
     c_pos = y * 80 + x;
 
-	outb(0x3d4, 0x0f);
-	outb(0x3d5, (uint8_t) c_pos);
-	outb(0x3d4, 0x0e);
-	outb(0x3d5, (uint8_t) (c_pos >> 8));
+    outb(0x3d4, 0x0f);
+    outb(0x3d5, (uint8_t)c_pos);
+    outb(0x3d4, 0x0e);
+    outb(0x3d5, (uint8_t)(c_pos >> 8));
 }
 
 void show_cursor(void)
 {
-	move_cursor(CURSOR_X, CURSOR_Y);
+    move_cursor(CURSOR_X, CURSOR_Y);
+}
+
+void console_service_keyboard()
+{
+    cputchar(READY_COLOR, get_ASCII_code_keyboard());
+    show_cursor();
+}
+
+void init_console()
+{
+    cclean();
+    kbd_init(); //Init keyboard
+    //init Video graphics here
 }
